2bd810e82dada41aaa602a861bf1d155aaa1911b
Daniel de Oliveira Colonheze <danielcolonhze@gmail.com>
2026-08-06 18:38:41 -0300

    commitando pra ver

--- aplication/components/hero/Hero.tsx
+++ aplication/components/hero/Hero.tsx
@@ -12,7 +12,7 @@ const textVariants = {
     x: 0,
     transition: {
       duration: 0.9,
-      ease: [0.22, 1, 0.36, 1],
+      ease: [0.22, 1, 0.36, 1] as const,
       staggerChildren: 0.12,
     },
   },
